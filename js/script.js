a0d57b9452686a113ab99ec9f8802de49c86c889
ImR0Ns <dariusrosu11@yahoo.com>
2021-08-30 15:12:38 +0300

    Pad card

--- js/script.js
+++ js/script.js
@@ -1,51 +1,48 @@
-function changeColors(key, clasa) {
-    $(document).on('keydown',function(e) {
-        if(e.which == key) {
-            $("." + clasa).css("color", "white");
-            $("." + clasa).css("background-color", "green");
-        }
-    });
-    $(document).on('keyup',function(e) {
-        if(e.which == key) {
-            $("." + clasa).css("color", "#8ee4af");
-            $("." + clasa).css("background-color", "white");
-        }
-    });
+var obj = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+var str = "";
+function add(id) {
+    $("." + id).on("click", function(){
+        if(id === "apply"){
+            if(str.length === 4){
+                str = "";
+                $(".pin-key").text("Pin code accepted!");
+            } else {
+                str = "";
+                $(".pin-key").text("You need 4 numbers to validate");
+            }
+        }else if(id === "deleteall") {
+            str = "";
+            $(".pin-key").text("You deleted everything");
+        }else if(id === "deleteone"){
+            str = str.substring(0, str.length - 1);
+            $(".pin-key").text(str);
+        }else if(str.length < 4){ // max length of pin
+            str += obj[id];
+            $(".pin-key").text(str);
+        } else { alert("Max 4 numbers!"); }
+    })
 }
 $(document).ready(function(){
-    //first row
-    changeColors(81, "q");
-    changeColors(87, "w");
-    changeColors(69, "e");
-    changeColors(82, "r");
-    changeColors(84, "t");
-    changeColors(89, "y");
-    changeColors(85, "u");
-    changeColors(73, "i");
-    changeColors(79, "o");
-    changeColors(80, "p");
-
-    //second row
-    changeColors(65, "a");
-    changeColors(83, "s");
-    changeColors(68, "d");
-    changeColors(70, "f");
-    changeColors(71, "g");
-    changeColors(72, "h");
-    changeColors(74, "j");
-    changeColors(75, "k");
-    changeColors(76, "l");
-
-    //last row
-    changeColors(90, "z");
-    changeColors(88, "x");
-    changeColors(67, "c");
-    changeColors(86, "v");
-    changeColors(66, "b");
-    changeColors(78, "n");
-    changeColors(77, "m");
-
-    //space
-    changeColors(32, "space");
-  
+    add("one");
+    add("two");
+    add("three");
+    add("four");
+    add("five");
+    add("six");
+    add("seven");
+    add("eight");
+    add("nine");
+    add("deleteall");
+    add("deleteone");
+    add("apply");
 })
